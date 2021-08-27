4ad745e363c5d142dba26a970708eefe42e3740c
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-08-27 10:29:15 -0300

    Fix Athlete model init

--- Atletas/models/Athlete.swift
+++ Atletas/models/Athlete.swift
@@ -19,9 +19,18 @@ class Athlete: User {
     var social: Social
     var projects: [Project]
     var sport: Sport
-    
-    init(name: String, username: String, password: String) {
-        super.init(username: username, password: password)
+
+    init(name: String, username: String, password: String, image: UIImage, about: String, type: Type, contact: String, pixKey: String, city: String, social: Social, projects: [Project], sport: Sport) {
         self.name = name
+        self.image = image
+        self.about = about
+        self.type = type
+        self.contact = contact
+        self.pixKey = pixKey
+        self.city = city
+        self.social = social
+        self.projects = projects
+        self.sport = sport
+        super.init(username: username, password: password)
     }
 }
