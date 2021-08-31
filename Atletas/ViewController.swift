0a0b91ce697b9ee4b6bffc16e107d0352840287d
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-08-31 09:35:03 -0300

    Setup swiftui preview

--- Atletas/ViewController.swift
+++ Atletas/ViewController.swift
@@ -18,3 +18,22 @@ class ViewController: UIViewController {
 
 }
 
+#if DEBUG
+import SwiftUI
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewDevice("iPhone 12")
+    }
+
+    struct ContentView: UIViewControllerRepresentable {
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return ViewController()
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            //
+        }
+    }
+}
+#endif
