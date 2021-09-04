619c14b96cc682cfeb6b454ba3421060d24ec7af
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-09-04 13:54:16 -0300

    Add image and button to ShowProjectVC

--- Atletas/screens/ShowProjectViewController/ShowProjectViewController.swift
+++ Atletas/screens/ShowProjectViewController/ShowProjectViewController.swift
@@ -9,7 +9,36 @@ import UIKit
 
 class ShowProjectViewController: UIViewController {
     lazy var supportButton: GreenRoundedButton = {
-        return GreenRoundedButton.getSupportButton()
+        let button = GreenRoundedButton.getSupportButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
+    }()
+
+    lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray
+
+        return view
+    }()
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 23, weight: .semibold)
+        label.textColor = .white
+
+        return label
+    }()
+
+    lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .white
+
+        return label
     }()
 
     var canEditProject = true
@@ -19,13 +48,8 @@ class ShowProjectViewController: UIViewController {
         view.backgroundColor = .systemBackground
 
         setupNavigationRightItens()
-
-        view.addSubview(supportButton)
-
-        supportButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
-        }
+        setupImageAndTitle()
+        setupButton()
     }
 
     override func viewDidLoad() {
@@ -41,6 +65,42 @@ class ShowProjectViewController: UIViewController {
             let editImage = UIImage(systemName: "pencil")
             let editButton = UIBarButtonItem(image: editImage, style: .plain, target: nil, action: nil)
             navigationItem.rightBarButtonItems?.append(editButton)
+        }
+    }
+
+    func setupImageAndTitle() {
+        titleLabel.text = "Sul Americano 2022"
+        subtitleLabel.text = "12 dias restantes"
+
+        view.addSubview(imageView)
+        view.addSubview(titleLabel)
+        view.addSubview(subtitleLabel)
+
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.width.equalToSuperview()
+            make.height.equalTo(300)
+        }
+
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView).offset(16)
+            make.leading.equalTo(imageView).offset(16)
+            make.trailing.equalTo(imageView).offset(-16)
+        }
+
+        subtitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
+            make.leading.equalTo(titleLabel)
+            make.trailing.equalTo(titleLabel)
+        }
+    }
+
+    func setupButton() {
+        view.addSubview(supportButton)
+
+        supportButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalTo(imageView.snp.bottom)
         }
     }
 }
