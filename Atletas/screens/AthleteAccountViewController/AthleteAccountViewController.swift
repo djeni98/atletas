faf734bd5e4893f804d593a00d60344bb1862fb4
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-09-05 17:05:50 -0300

    Create AccountHeader for AthleteAccountVC

---
+++ Atletas/screens/AthleteAccountViewController/AthleteAccountViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AthleteAccountViewController.swift
+//  Atletas
+//
+//  Created by Djenifer Renata Pereira on 05/09/21.
+//
+
+import UIKit
+
+class AthleteAccountViewController: UIViewController {
+    lazy var accountHeaderView: AccountHeaderView = {
+        let view = AccountHeaderView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .systemBackground
+        setup()
+    }
+
+    func setup() {
+        view.addSubview(accountHeaderView)
+
+        accountHeaderView.snp.makeConstraints { make in
+            make.top.equalTo(view.layoutMarginsGuide.snp.top).offset(32)
+
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+        }
+    }
+
+}
+
+#if DEBUG
+import SwiftUI
+struct AthleteAccountViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 12")
+    }
+
+    struct ContentView: UIViewControllerRepresentable {
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            let vc = AthleteAccountViewController()
+            let navigationController = UINavigationController(rootViewController: vc)
+
+            return navigationController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            //
+        }
+    }
+}
+#endif
