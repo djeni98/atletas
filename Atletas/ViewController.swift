2a5125fd9f810ea5b4a1aca4d66a6d00941eb345
schueda <a.schuedamenezes@gmail.com>
2021-09-10 09:11:10 -0300

    Voltando a ViewController ao normal

--- Atletas/ViewController.swift
+++ Atletas/ViewController.swift
@@ -9,47 +9,8 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    
-    lazy var button: GreenRoundedButton = {
-        let button = GreenRoundedButton.getSupportButton()
-        button.addTarget(self, action: #selector(pressed), for: .touchUpInside)
-        return button
-    }()
-    
-    @objc func pressed() {
-        navigationController?.present(UINavigationController(rootViewController: ProjectViewController()), animated: true)
-    }
-    
     override func loadView() {
         super.loadView()
-        
-        view.addSubview(button)
-        button.snp.makeConstraints { make in
-            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-32)
-            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
-            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
-        }
-        
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.title = "Maequisnho"
-    }
-}
 
-#if DEBUG
-import SwiftUI
-struct ViewController_Preview: PreviewProvider {
-    static var previews: some View {
-        ContentView().previewDevice("iPhone 12")
-    }
-    
-    struct ContentView: UIViewControllerRepresentable {
-        
-        func makeUIViewController(context: Context) -> UIViewController {
-            UINavigationController(rootViewController: ViewController())
-        }
-        
-        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        }
     }
 }
-#endif
