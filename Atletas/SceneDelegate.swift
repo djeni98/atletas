071054d6fd6af5bd2d986a86737f95f4675aa81f
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-09-10 10:00:29 -0300

    Create tab bar

--- Atletas/SceneDelegate.swift
+++ Atletas/SceneDelegate.swift
@@ -23,8 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
         
-        let nav = UINavigationController(rootViewController: ViewController())
-        window?.rootViewController = nav
+        window?.rootViewController = AppTabBarController()
         
     }
 
