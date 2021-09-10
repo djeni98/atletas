d11aaf43ef7ada2c0d9f8b70dca38d42c43103c3
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-09-10 17:51:56 -0300

    Remove largeTitle in ShowProjectVC

--- Atletas/Screens/ShowProjectViewController/ShowProjectViewController.swift
+++ Atletas/Screens/ShowProjectViewController/ShowProjectViewController.swift
@@ -98,6 +98,16 @@ class ShowProjectViewController: UIViewController {
         setup()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
     func setup() {
         setupNavigationRightItens()
 
